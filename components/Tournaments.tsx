f1044d61e5a6ee4708252ec8f4b16bd83a26f847
adrian-petrykiw <cucumber901@gmail.com>
2024-01-11 17:05:04 -0600

    formatting

--- components/Tournaments.tsx
+++ components/Tournaments.tsx
@@ -15,14 +15,15 @@ import duration from "dayjs/plugin/duration";
 import Link from "next/link";
 import userStore from "@/stores/userStore";
 import { IoMdTrophy } from "react-icons/io";
-import { MdPerson } from "react-icons/md";
+import { MdArrowBack, MdPerson } from "react-icons/md";
 import { FaCopy, FaShare } from "react-icons/fa";
 import toast from "react-hot-toast";
+import { useRouter } from "next/router";
+import Image from "next/image";
 
 dayjs.extend(duration);
 
 export function Tournaments() {
-  const [tournaments, setTournaments] = useState<Tournament[]>([]);
   const {
     username,
     loggedIn,
@@ -32,12 +33,29 @@ export function Tournaments() {
     userProfilePic,
   } = userStore();
 
-  useEffect(() => {
-    getTournamentsByGameName("Meteor Crash").then((data: Tournament[]) => {
-      setTournaments(data);
-    });
-  }, []);
+  const [tournaments, setTournaments] = useState<Tournament[]>([]);
+  const [selectedTournament, setSelectedTournament] =
+    useState<Tournament | null>(null);
+  const [creatingTournament, setCreatingTournament] = useState<boolean>(false);
+  const router = useRouter();
 
+  useEffect(() => {
+    getTournamentsByGameName("Bounceback Reload").then((data: Tournament[]) => {
+      setTournaments(data);
+      // Check for tournament ID in URL and update state
+      const tournamentId = router.query.tournamentId;
+      if (tournamentId) {
+        const tournament = data.find((t) => t.id.toString() === tournamentId);
+        if (tournament) setSelectedTournament(tournament);
+      }
+    });
+  }, [router.query.tournamentId]);
+
+  const selectTournament = (tournament: Tournament) => {
+    setSelectedTournament(tournament);
+    // Update URL without navigating
+    router.push(`?tournamentId=${tournament.id}`, undefined, { shallow: true });
+  };
   const formatNumber = (num: number) => {
     return num.toString().padStart(2, "0");
   };
@@ -79,6 +97,387 @@ export function Tournaments() {
     return `${name.substring(0, 3)}...${name.substring(name.length - 5)}`;
   };
 
+  if (selectedTournament) {
+    // Render Tournament Details View
+    return (
+      <Flex
+        flexDirection="column"
+        justifyContent="space-between"
+        align="center"
+        h="100%"
+      >
+        <Flex
+          w="100%"
+          h="100%"
+          flexDirection="column"
+          justifyContent="start"
+          align="start"
+        >
+          <Flex w="100%" h="2rem" justifyContent="start" align="center">
+            <Flex
+              color="white"
+              _hover={{ color: "rgba(255, 255, 255, 0.8)" }}
+              flex={1}
+            >
+              <MdArrowBack
+                size="1.25rem"
+                style={{
+                  marginLeft: "0px",
+                  cursor: "pointer",
+                  _hover: `{ color: "rgba(255, 255, 255, 0.8)" }`,
+                }}
+                onClick={() => setSelectedTournament(null)}
+              />
+            </Flex>
+            <Flex flex={10}>
+              <Text
+                color="#fbfbfb"
+                fontSize="1rem"
+                fontWeight="700"
+                textAlign="center"
+                width="100%"
+              >
+                {selectedTournament.tournament_name}
+              </Text>
+            </Flex>
+            <Flex flex={1} bg="blue"></Flex>
+          </Flex>
+
+          <Flex
+            w="100%"
+            h="1rem"
+            justifyContent="space-between"
+            align="center"
+            mt="0.5rem"
+          >
+            <Flex>
+              <Tooltip
+                label="Creation Date"
+                aria-label="Creation Date"
+                bg="black"
+                border="1px solid white"
+              >
+                <Text
+                  color={theme.colors.primary}
+                  fontSize="0.75rem"
+                  overflow="auto"
+                  fontWeight="500"
+                  textAlign="start"
+                  width="100%"
+                >
+                  {selectedTournament.start_datetime}
+                </Text>
+              </Tooltip>
+            </Flex>
+
+            <Tooltip
+              label="Tournament Members"
+              aria-label="Tournament Members"
+              bg="black"
+              border="1px solid white"
+            >
+              <Flex justifyContent="flex-end">
+                <MdPerson size="1rem" />
+                <Text
+                  color={theme.colors.primary}
+                  fontSize="0.7rem"
+                  fontWeight="700"
+                  ml="0.25rem"
+                >
+                  4/4
+                </Text>
+              </Flex>
+            </Tooltip>
+          </Flex>
+          <Flex
+            w="100%"
+            h="2rem"
+            justifyContent="space-between"
+            align="center"
+            mt="0.5rem"
+            gap="0.75rem"
+          >
+            <Flex
+              justifyContent="center"
+              align="center"
+              bg={theme.colors.input}
+              w="100%"
+              h="100%"
+              px="0.5rem"
+              py="1rem"
+              borderRadius="2px"
+              flex={5}
+            >
+              <Text
+                fontSize="0.7rem"
+                fontWeight="800"
+                color={theme.colors.primary}
+              >
+                PRIZE POOL:
+                {/* {`${tournament.creator}`} */}
+              </Text>
+              <Text
+                ml="0.25rem"
+                fontSize="0.7rem"
+                fontWeight="800"
+                color={theme.colors.green}
+              >
+                ${`222`}.00
+              </Text>
+            </Flex>
+            <Flex
+              justifyContent="center"
+              align="center"
+              bg={theme.colors.input}
+              w="100%"
+              h="100%"
+              px="0.5rem"
+              py="0.25rem"
+              borderRadius="2px"
+              flex={5}
+            >
+              {/* <Text
+                textAlign="center"
+                fontSize="0.7rem"
+                fontWeight="800"
+                color={theme.colors.pink}
+              >
+                AWAITING PLAYERS
+              </Text>{" "} */}
+              {/* <Text
+                textAlign="center"
+                fontSize="0.7rem"
+                fontWeight="800"
+                color={theme.colors.orange}
+              >
+                IN PROGRESS
+              </Text> */}
+              <Text
+                textAlign="center"
+                fontSize="0.7rem"
+                fontWeight="800"
+                color={theme.colors.green}
+              >
+                WINNINGS AVAILABLE
+              </Text>
+              {/* 
+              <Text
+                textAlign="center"
+                fontSize="0.7rem"
+                fontWeight="800"
+                color={theme.colors.darkerGray}
+              >
+                FINISHED
+              </Text> */}
+            </Flex>
+          </Flex>
+          <Flex
+            flexDirection="column"
+            justifyContent="center"
+            align="center"
+            bg={theme.colors.input}
+            w="100%"
+            h="100%"
+            my="0.75rem"
+            px="0.75rem"
+            borderRadius="2px"
+            flex={5}
+          >
+            <Flex
+              w="100%"
+              h="10%"
+              justifyContent="space-between"
+              align="start"
+              mb="0.5rem"
+            >
+              <Text
+                fontSize="0.7rem"
+                fontWeight="800"
+                color={theme.colors.primary}
+              >
+                LEADERBOARD
+                {/* {`${tournament.creator}`} */}
+              </Text>
+              <Text
+                ml="0.25rem"
+                fontSize="0.7rem"
+                fontWeight="800"
+                color={theme.colors.primary}
+              >
+                SCORE
+              </Text>
+            </Flex>
+
+            <List
+              width="100%"
+              h="8.25rem"
+              justifyContent="space-between"
+              overflowY="auto"
+            >
+              <Flex flexDirection="column" w="100%" h="100%" gap="0.5rem">
+                <Flex h="2rem" align="center" justifyContent="space-between">
+                  <Flex align="center">
+                    {" "}
+                    <Text
+                      fontSize="0.7rem"
+                      fontWeight="800"
+                      color={theme.colors.primary}
+                      mr="0.5rem"
+                    >
+                      1.
+                      {/* {`${tournament.creator}`} */}
+                    </Text>
+                    <Box alignItems="center" mr="0.5rem">
+                      <Image
+                        src={"/assets/ball.png"}
+                        alt="Default ball image"
+                        width={25}
+                        height={25}
+                        style={{ borderRadius: "50%" }}
+                      />
+                    </Box>
+                    <Text
+                      fontSize="0.7rem"
+                      fontWeight="800"
+                      color={theme.colors.primary}
+                      mr="0.5rem"
+                    >
+                      ...
+                      {/* {`${tournament.creator}`} */}
+                    </Text>
+                  </Flex>
+                  <Text
+                    fontSize="0.7rem"
+                    fontWeight="500"
+                    fontStyle="italic"
+                    color={theme.colors.primary}
+                    mr="0.5rem"
+                  >
+                    0
+                  </Text>
+                </Flex>
+              </Flex>
+            </List>
+          </Flex>
+          <Button
+            mb="0.5rem"
+            bg={theme.colors.green}
+            color="white"
+            width="100%"
+            borderColor={theme.colors.green}
+            borderWidth="2px"
+            borderRadius="1px"
+            h="2.25rem"
+            w="100%"
+            fontSize="0.8rem"
+            fontWeight="700"
+            isDisabled={false}
+            _hover={{
+              bg: theme.colors.darkerGreen,
+              borderColor: theme.colors.darkerGreen,
+            }}
+            onClick={() => setCreatingTournament(true)}
+          >
+            JOIN FOR ${selectedTournament.id}.00
+          </Button>
+        </Flex>
+      </Flex>
+    );
+  }
+
+  if (creatingTournament) {
+    // Render Tournament Details View
+    return (
+      <Flex
+        flexDirection="column"
+        justifyContent="space-between"
+        align="center"
+        h="100%"
+      >
+        <Flex
+          w="100%"
+          h="100%"
+          flexDirection="column"
+          justifyContent="start"
+          align="space-between"
+        >
+          <Text
+            color="#fbfbfb"
+            fontSize="1.25rem"
+            fontWeight="700"
+            textAlign="start"
+            width="100%"
+          >
+            TOURNAMENTS
+          </Text>
+          <Flex
+            w="100%"
+            h="2rem"
+            justifyContent="start"
+            align="center"
+            mt="1rem"
+          >
+            <Text
+              color="#fbfbfb"
+              fontSize="0.9rem"
+              overflow="auto"
+              fontWeight="700"
+              textAlign="center"
+              width="100%"
+              // bg="red"
+              mx="0.5rem"
+            >
+              CREATE
+            </Text>
+          </Flex>
+          <Flex w="100%" gap="0.75rem">
+            <Button
+              bg={theme.colors.red}
+              color="white"
+              width="100%"
+              borderColor={theme.colors.red}
+              borderWidth="2px"
+              borderRadius="1px"
+              h="2.25rem"
+              w="100%"
+              fontSize="0.8rem"
+              fontWeight="700"
+              isDisabled={false}
+              _hover={{ bg: "#ea0000", borderColor: "#ea0000" }}
+              onClick={() => setCreatingTournament(false)}
+            >
+              CANCEL
+            </Button>
+            <Button
+              bg={theme.colors.green}
+              color="white"
+              width="100%"
+              borderColor={theme.colors.green}
+              borderWidth="2px"
+              borderRadius="1px"
+              h="2.25rem"
+              w="100%"
+              fontSize="0.8rem"
+              fontWeight="700"
+              isDisabled={false}
+              // onClick={() => {
+              //   console.log("check 1 ran");
+              //   handleSubmit(handleGenerateBallImage);
+              // }}
+              type="submit"
+              _hover={{
+                bg: theme.colors.darkerGreen,
+                borderColor: theme.colors.darkerGreen,
+              }}
+            >
+              CREATE
+            </Button>
+          </Flex>
+        </Flex>
+      </Flex>
+    );
+  }
+
   return (
     <Flex
       flexDirection="column"
@@ -89,7 +488,7 @@ export function Tournaments() {
       <Flex w="100%" h="100%">
         <Text
           color="#fbfbfb"
-          fontSize="21px"
+          fontSize="1.25rem"
           fontWeight="700"
           textAlign="start"
           width="100%"
@@ -109,6 +508,16 @@ export function Tournaments() {
                 mb={3}
                 backgroundColor="#1A1A1D"
                 cursor="pointer"
+                onClick={() =>
+                  selectTournament({
+                    id: 1,
+                    game_name: "name",
+                    tournament_name: "tourn name",
+                    start_datetime: "start",
+                    end_datetime: "end",
+                    tournament_link: "link",
+                  })
+                }
               >
                 <VStack
                   h="100%"
@@ -269,6 +678,8 @@ export function Tournaments() {
             //         mb={3}
             //         backgroundColor="#1A1A1D"
             //         cursor="pointer"
+            // onClick={() => selectTournament(tournaments[i])}
+
             //       >
             //         <VStack align="start" spacing={1}>
             //           <Text fontSize="14px" color="#fbfbfb" fontWeight="bold">
@@ -302,10 +713,10 @@ export function Tournaments() {
           fontWeight="700"
           isDisabled={false}
           _hover={{
-            bg: theme.colors.greenhighlight,
-            borderColor: theme.colors.greenhighlight,
+            bg: theme.colors.darkerGreen,
+            borderColor: theme.colors.darkerGreen,
           }}
-          onClick={() => {}}
+          onClick={() => setCreatingTournament(true)}
         >
           CREATE NEW +
         </Button>
--- utils/tournament.ts
+++ utils/tournament.ts
@@ -103,3 +103,150 @@ export async function startTournament(
     throw error;
   }
 }
+
+export async function getMatch(
+  matchPubkey: string,
+  wallet: Wallet,
+  connection: Connection
+): Promise<any> {
+  // Replace any with the appropriate type
+  const provider = new AnchorProvider(connection, wallet, {});
+  const authority = new Solana.Http.Tournaments.Client(
+    provider,
+    "mainnet-beta"
+  );
+
+  try {
+    const matchData = await authority.getMatch(matchPubkey);
+    console.log("Match Data:", matchData);
+    return matchData;
+  } catch (error) {
+    console.error("Error getting match data:", error);
+    throw error;
+  }
+}
+
+export async function getMatchesByRound(
+  tournamentPubkey: string,
+  roundIndex: number,
+  finalized: boolean,
+  wallet: Wallet,
+  connection: Connection
+): Promise<any[]> {
+  // Replace any[] with the appropriate type
+  const provider = new AnchorProvider(connection, wallet, {});
+  const authority = new Solana.Http.Tournaments.Client(
+    provider,
+    "mainnet-beta"
+  );
+
+  try {
+    const matches = await authority.getMatchesByRound(
+      tournamentPubkey,
+      roundIndex,
+      finalized
+    );
+    console.log("Matches:", matches);
+    return matches;
+  } catch (error) {
+    console.error("Error getting matches by round:", error);
+    throw error;
+  }
+}
+
+export async function getMatchParticipants(
+  tournamentPubkey: string,
+  matchPubkey: string,
+  wallet: Wallet,
+  connection: Connection
+): Promise<string[]> {
+  const provider = new AnchorProvider(connection, wallet, {});
+  const authority = new Solana.Http.Tournaments.Client(
+    provider,
+    "mainnet-beta"
+  );
+
+  try {
+    const participants = await authority.getMatchParticipants(
+      tournamentPubkey,
+      matchPubkey
+    );
+    console.log("Participants:", participants);
+    return participants;
+  } catch (error) {
+    console.error("Error getting match participants:", error);
+    throw error;
+  }
+}
+
+export async function getParticipantsForRound(
+  tournamentPubkey: string,
+  roundIndex: number,
+  wallet: Wallet,
+  connection: Connection
+): Promise<string[]> {
+  const provider = new AnchorProvider(connection, wallet, {});
+  const authority = new Solana.Http.Tournaments.Client(
+    provider,
+    "mainnet-beta"
+  );
+
+  try {
+    const participants = await authority.getParticipantsForRound(
+      tournamentPubkey,
+      roundIndex
+    );
+    console.log("Participants for round:", participants);
+    return participants;
+  } catch (error) {
+    console.error("Error getting participants for round:", error);
+    throw error;
+  }
+}
+
+export async function getTournamentStandings(
+  tournamentPubkey: string,
+  wallet: Wallet,
+  connection: Connection
+): Promise<Map<string, number>> {
+  // Create an Anchor Provider
+  const provider = new AnchorProvider(connection, wallet, {});
+  // Initialize the tournament authority
+  const authority = new Solana.Http.Tournaments.Client(
+    provider,
+    "mainnet-beta"
+  );
+
+  try {
+    const standings = await authority.getStandings(tournamentPubkey);
+    console.log("Tournament standings retrieved successfully");
+    return standings;
+  } catch (error) {
+    console.error("Error retrieving tournament standings:", error);
+    throw error;
+  }
+}
+
+export async function getTournamentDetails(
+  tournamentPubkey: string,
+  wallet: Wallet,
+  connection: Connection
+): Promise<any> {
+  // Replace 'any' with the appropriate type for tournament details
+  // Create an Anchor Provider
+  const provider = new AnchorProvider(connection, wallet, {});
+  // Initialize the tournament authority
+  const authority = new Solana.Http.Tournaments.Client(
+    provider,
+    "mainnet-beta"
+  );
+
+  try {
+    const tournamentData = await authority.getTournament(tournamentPubkey);
+    console.log("Tournament details retrieved successfully");
+    return tournamentData;
+  } catch (error) {
+    console.error("Error retrieving tournament details:", error);
+    throw error;
+  }
+}
